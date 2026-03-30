aa7127330c0803019613554c0a71d10b75337763
pepeps <gpe9503@hotmail.com>
2026-03-30 20:49:28 +0100

    Create a new maven project, Add GRPC dependencies, Add first inventory proto file

---
+++ src/main/java/sg12/sg12/smartfoodwastesystem/SG12SmartFoodWasteSystem.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package sg12.sg12.smartfoodwastesystem;
+
+/**
+ *
+ * @author joseperez
+ */
+public class SG12SmartFoodWasteSystem {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
